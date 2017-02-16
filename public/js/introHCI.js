51b00f19afbb30be102605515ff5b7925983eba1
Christopher Wong <chris@Christophers-MacBook-Pro.local>
2017-02-16 14:44:48 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -14,6 +14,7 @@ function initializePage() {
 	$('#colorBtn').click(randomizeColors);
 }
 
+var currentID;
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
@@ -25,8 +26,20 @@ function addProjectDetails(e) {
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
-
+	currentID = projectID;
 	console.log("User clicked on project " + idNumber);
+	$.get('/project/' + idNumber, addProject);
+
+}
+
+function addProject(results){
+	var projectHTML = 
+    '<img src="' + results['image'] + '" class="detailsImage">' +
+    '<p>' + results['title'] + results["summary"] + '</p>' +
+    '<p><small>' + results['date'] +
+    '</small></p>'; 
+console.log(results);
+$("#" + currentID + "details").html(projectHTML);
 }
 
 /*
@@ -35,4 +48,17 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get('/palette', spectrum);
+
+}
+
+function spectrum(result){
+	console.log(result.colors.hex);
+	var colors = result.colors.hex;
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+
 }
